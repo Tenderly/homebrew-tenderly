ae9cc601bd68d9fea23bf8ec0864438bff5fe6f9
Cloud Build <cloudbuild@tenderly.app>
2018-09-26 18:57:05 +0000

    Publishing build c5cfdb34012d50b87bad1430951223278844f5f8

--- tenderly.rb
+++ tenderly.rb
@@ -2,7 +2,7 @@ class Tenderly < Formula
   desc "Tenderly CLI"
   homepage "https://github.com/tenderly"
   url "https://storage.googleapis.com/tenderly-cli/tenderly.tar.gz"
-  sha256 "d0b7fbce598fd7e4a78d24d1306d6dd080e5b020c4745f65ac3ccf667151a30e"
+  sha256 "b26810f9fb6bb5400c3653d18c876c3a433f2028ed35e4a6040d13a3779c9ff4"
   version "0.0.1"
 
   bottle :unneeded
